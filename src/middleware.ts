51f73d7ac677a59c4c6adf4068f99433c3cf6556
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2025-08-25 20:07:30 +0300

    clerk matcher change

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,9 @@ export default clerkMiddleware();
 
 export const config = {
     matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
+        // Protect everything except:
+        // - Next.js internals (_next, static files)
+        // - Clerk API routes
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)|api/(clerk|webhooks)).*)',
     ],
 };
